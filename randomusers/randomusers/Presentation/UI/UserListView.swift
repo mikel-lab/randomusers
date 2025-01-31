a1af26b4de989a1bc9243a2724e946fb8216338f
Mikel <mikelcobian@gmail.com>
2025-01-31 12:59:06 +0100

    feature: folder structure applied to project

---
+++ randomusers/randomusers/Presentation/UI/UserListView.swift
@@ -0,0 +1,18 @@
+//
+//  UserListView.swift
+//  randomusers
+//
+//  Created by Mikel Cobian on 31/1/25.
+//
+
+import SwiftUI
+
+struct UserListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserListView()
+}
