60772de10ca103b58d7ff743cf3b98de9e2dd0a3
Mikel <mikelcobian@gmail.com>
2025-02-03 10:27:16 +0100

    feature: added a collection for duplicated users to avoid extra users on final list

--- randomusers/randomusers/Presentation/ViewModels/UserListViewModel.swift
+++ randomusers/randomusers/Presentation/ViewModels/UserListViewModel.swift
@@ -9,13 +9,14 @@ import Foundation
 
 @MainActor
 class UserListViewModel: ObservableObject {
-	@Published private(set) var users: [UserModel] = []
-	@Published private(set) var isLoading = false
-	@Published private(set) var error: Error?
+	@Published var users: [UserModel] = []
+	@Published var isLoading = false
+	@Published var error: Error?
 	
 	private let userService: UserServiceProtocol
 	private var currentPage = 1
 	private var isFetching = false
+	private var emailSet: Set<String> = [] // Track unique emails
 	
 	init(userService: UserServiceProtocol = UserService()) {
 		self.userService = userService
@@ -30,7 +31,18 @@ class UserListViewModel: ObservableObject {
 		
 		do {
 			let newUsers = try await userService.fetchUsers(page: currentPage)
-			users.append(contentsOf: newUsers)
+			
+			// Filter out duplicate users based on email
+			let uniqueNewUsers = newUsers.filter { user in
+				!emailSet.contains(user.email)
+			}
+			
+			// Update email set and users array
+			uniqueNewUsers.forEach { user in
+				emailSet.insert(user.email)
+			}
+			users.append(contentsOf: uniqueNewUsers)
+			
 			currentPage += 1
 		} catch {
 			self.error = error
@@ -42,7 +54,7 @@ class UserListViewModel: ObservableObject {
 	
 	func loadMoreIfNeeded(currentUser user: UserModel) async {
 		let thresholdIndex = users.index(users.endIndex, offsetBy: -5)
-		if let userIndex = users.firstIndex(where: { $0.email == user.email }), 
+		if let userIndex = users.firstIndex(where: { $0.email == user.email }),
 		   userIndex == thresholdIndex {
 			await fetchUsers()
 		}
